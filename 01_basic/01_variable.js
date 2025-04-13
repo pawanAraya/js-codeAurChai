054608112198cbc6d383eb62136061f72fc536e0
Pawan Kumar <pawankumarbella123@gmail.com>
2025-04-13 07:46:18 +0000

    conatiner config

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accuntId = 144553;
+let accuntEmail = "Pawan123@gmail.com";
+var accuntPassword = "2345";
+accuntCity = "Mumbai";
+console.table([accuntId , accuntEmail , accuntPassword , accuntCity]);
+
+// accuntId = 2 // not allowed
+
+accuntEmail = "Hitesh@gmail.com";
+accuntCity = "Jaipur";
+console.table([accuntId , accuntEmail , accuntPassword , accuntCity]);
+
